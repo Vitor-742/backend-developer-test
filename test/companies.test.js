48a55a82a530fc68716a84a466f2ce1ee359da23
vitor <vitor.campos@autaza.com>
2024-02-15 10:40:25 -0300

    testes para GET /companies/:id

--- test/companies.test.js
+++ test/companies.test.js
@@ -3,8 +3,6 @@ import request from 'request';
 
 const expect = chai.expect;
 
-import { app } from '../src/api.js';
-
 describe('Companies test', () => {
   describe('GET /companies', () => {
     context('successful request', () => {
@@ -40,6 +38,59 @@ describe('Companies test', () => {
 
         // verifica se o body tem o tamanho esperado
         expect(JSON.parse(body)).to.have.lengthOf(3);
+        
+        done()
+      })
+
+      it('expected properties', (done) => {
+
+        // verifica se o body tem as propriedades esperadas
+        expect(JSON.parse(body)[0]).to.have.property('id');
+        expect(JSON.parse(body)[0]).to.have.property('name');
+        expect(JSON.parse(body)[0]).to.have.property('created_at');
+        expect(JSON.parse(body)[0]).to.have.property('updated_at');
+        
+        done()
+      })
+      })
+    });
+  });
+  describe('GET /companies/:company_id', () => {
+    context('successful request', () => {
+
+      // requisicao http    
+      request('http://localhost:3000/companies/7034a6b4-99d4-4e34-8fb5-527ac6eeffe4', (error, response, body) => {
+
+      it('no error', (done) => {
+
+        // verifica se há erro
+        expect(error).to.equal(null);
+
+        done()
+      })
+
+      it('return status 200', (done) => {
+
+        // verifica se o status da requisicao e OK
+        expect(response.statusCode).to.equal(200);
+        
+        done()
+      })
+
+      it('return correct type', (done) => {
+
+        // verifica se o body esta no formato esperado
+        expect(JSON.parse(body)).to.be.an('array');
+        
+        done()
+      })
+
+      console.log(body)
+
+      it('expected length', (done) => {
+
+        // verifica se o body tem o tamanho esperado
+        expect(JSON.parse(body)).to.have.lengthOf(1);
         
         done()
       })
--- test/resetDatabase.js
+++ test/resetDatabase.js
@@ -6,7 +6,7 @@ try {
     await client.query('DELETE FROM companies');
     await client.query('DELETE FROM jobs');
     await client.query(`  
-        insert into companies (name) values ('ABC Corp');
+        insert into companies (id, name) values ('7034a6b4-99d4-4e34-8fb5-527ac6eeffe4', 'ABC Corp');
         insert into companies (name) values ('XYZ LLC');
         insert into companies (name) values ('ACME Enterprises');
     `);
