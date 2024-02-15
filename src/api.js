ab2d61f53095b065dbd8530274127942d0cfc715
vitor <vitor.campos@autaza.com>
2024-02-15 09:31:22 -0300

    retirando linhas comentadas

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import express from 'express'
 const app = express()
 
-//const companiesController = require('./controllers/companiesController');
 import { companiesRouter } from './controllers/companiesController.js';
 
 app.use(express.json());
@@ -11,5 +10,4 @@ app.use(express.json());
 app.use('/companies', companiesRouter);
 
 // Importando app para que index.js possa usa-lo
-//module.exports = app;
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// import dotenv from 'dotenv';
-// dotenv.config({ path: '../.env' });
-
 import 'dotenv/config'
 
 import { app } from './api.js'
-//import app from './api';
 
 const port = 3000;
 
