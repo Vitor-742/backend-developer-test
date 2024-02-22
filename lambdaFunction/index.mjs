ceff6e57cdc77634b999dd4ed8d86ab1d9edc21c
vitor <vitor.campos@autaza.com>
2024-02-21 23:38:58 -0300

    organizando arquivo index lambda

--- lambdaFunction/index.mjs
+++ lambdaFunction/index.mjs
@@ -3,6 +3,7 @@ const { Pool } = pkg;
 import AWS from 'aws-sdk';
 const s3 = new AWS.S3();
 export const handler = async () => {
+  
     // pool de conexao com banco de dados
     const pool = new Pool({
         user: process.env.DB_USER,
@@ -14,28 +15,36 @@ export const handler = async () => {
             rejectUnauthorized: false
         }
     });
+
+    // conexao com client
     const client = await pool.connect();
+
+    // constroi e executa query
     const query = `SELECT * FROM jobs WHERE status = 'published';`;
     const result = await client.query(query);
-    console.log(JSON.parse(result));
-    const bucketName = 'vitor-campos-bucket';
-    const key = 's3://vitor-campos-bucket/feed.json';
 
+    // dados do bucket
     const params = {
-        Bucket: bucketName,
-        Key: key,
+        Bucket: 'vitor-campos-bucket',
+        Key: 's3://vitor-campos-bucket/feed.json',
         Body: result,
     };
+
     try {
         // Reescrever o arquivo no S3
         const result = await s3.putObject(params).promise();
+
         console.log('Arquivo reescrito com sucesso');
+
         return {
           statusCode: 200,
           body: 'Arquivo reescrito com sucesso.',
         };
+
       } catch (error) {
+
         console.error('Erro ao reescrever o arquivo:', error);
+        
         return {
           statusCode: 500,
           body: 'Erro ao reescrever o arquivo.',
